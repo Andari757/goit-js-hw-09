10de40628e60545d39a24fd1ffe9bfb1e4f65822
Andari757 <matviyskrypka@gmail.com>
2022-04-11 19:35:40 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+const input = document.querySelector("#datetime-picker")
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -7,5 +9,28 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    // console.log(Date()-selectedDates[0], Date())
+    
   },
 };
+flatpickr(input, options) 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
