af394fff416340b2649d4b83f1f1156e37e31feb
Andari757 <matviyskrypka@gmail.com>
2022-04-21 11:08:17 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,11 @@
-function createPromise(position, delay) {
-  console.log(delay)
+function createPromise(position, delay) {  
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-    resolve(position,delay)
+    resolve({position,delay})
   } else {
-    reject(position, delay)
+    reject({position, delay})
   }  
     }, delay)})
   
